671b2b208744a8325c97356b6d620862bdb4f38b
Katharine Lundblad <klundblad@live.com>
2021-04-24 12:31:38 -0700

    Add files via upload

---
+++ PythonDataStream.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+PC app for Data Stream from the ESP32 
+Created on Sun Jan 31 10:27:57 2021
+
+@author: kathy
+"""
+import socket
+
+s = socket.socket()
+s.bind(('0.0.0.0', 8090))
+s.listen(0)
+
+while True:
+    
+    client, addr = s.accept()
+    # client handling code
+    
+    while True:
+        content = client.recv(32)
+        
+        if len(content) == 0:
+            break
+        else:
+            print(content)
+            
+    print("Closing connection")
+    client.close()
+    
